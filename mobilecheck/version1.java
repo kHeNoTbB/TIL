bded01e9d1898baa8b618a0f944d895ce6743105
Hailey <hailey.kim.dev@gmail.com>
2020-10-30 10:12:51 +0900

    Update version1.java

--- mobilecheck/version1.java
+++ mobilecheck/version1.java
@@ -2,8 +2,15 @@ protected boolean isMobileCheck(HttpSerVletRequest req) {
   String userAgent = req.getHeader("User=Agent");
   bolean isMobile  = false;
   
-  if(userAgent!=null && userAgent.toLoWerCase().replace(' ', '%').matcheds("패턴에 맞게 입력")) isMobile = true;
+  if(userAgent!=null && userAgent.toLoWerCase().replace(' ', '%').matches("패턴에 맞게 입력")) isMobile = true;
   
   return isMobile;
 }
   
+/*
+  + "(morpheus/nt|nokia|iphone|ipad|android|motorola|^mot-|softbank|foma|docomo|kddi|up.browser|up.link|"
+  + "htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|"
+  + "blackberry|alcate|amoi|ktouch|nexian|samsung|^sam-|s[cglh|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|"
+  + "symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte-|longcos|pantech|gionee|^sie-|portalmmm|"
+  + "jigs browser|hiptop|^ucweb|^beng|haier|^lct|operas*mobi|opera*mini|320x320|240x320|176x220)"
+*/
